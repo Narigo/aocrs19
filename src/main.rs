af8e4c31638080405e900b3546581a412c4bc251
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-12-03 00:57:26 +0100

    Add solutions for day2
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod aoc01;
+mod aoc02;
 
 fn main() {
     println!("Hello, world!");
     println!("aoc01_01 = {}", aoc01::sum_fuel_01());
     println!("aoc01_02 = {}", aoc01::sum_fuel_02());
+    println!("aoc02_01 = {}", aoc02::program_1202_01());
+    println!("aoc02_02 = {}", aoc02::program_1202_02());
 }
